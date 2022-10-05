5e7c35e002966a66df04c5ae9be2d00f4716014d
phankhacdien <phankhacdien1110@gmail.com>
2022-10-05 22:12:06 +0700

    update branch Spring Secu

---
+++ Code JAVA/2. Java Web/02_Spring_Security/Security_Viblo_NguyenHoangNam/src/main/java/com/example/security_viblo_nguyenhoangnam/SecurityVibloNguyenHoangNamApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_viblo_nguyenhoangnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityVibloNguyenHoangNamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityVibloNguyenHoangNamApplication.class, args);
+    }
+
+}
---
+++ Code JAVA/2. Java Web/02_Spring_Security/Security_Viblo_NguyenHoangNam/src/test/java/com/example/security_viblo_nguyenhoangnam/SecurityVibloNguyenHoangNamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_viblo_nguyenhoangnam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityVibloNguyenHoangNamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
