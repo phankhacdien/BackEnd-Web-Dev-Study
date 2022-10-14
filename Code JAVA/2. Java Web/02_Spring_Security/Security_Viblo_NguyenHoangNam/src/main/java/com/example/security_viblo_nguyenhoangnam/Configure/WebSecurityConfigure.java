76ec9f6142a9a0374c1fb5663192c0b0332f93a4
PhanKhacDien <phankhacdien1110@gmail.com>
2022-10-14 15:44:50 +0700

    configure H2 database for Security_Viblo_NguyenHoangNam in application/properties

---
+++ Code JAVA/2. Java Web/02_Spring_Security/Security_Viblo_NguyenHoangNam/src/main/java/com/example/security_viblo_nguyenhoangnam/Configure/WebSecurityConfigure.java
@@ -0,0 +1,9 @@
+package com.example.security_viblo_nguyenhoangnam.Configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    
+}
