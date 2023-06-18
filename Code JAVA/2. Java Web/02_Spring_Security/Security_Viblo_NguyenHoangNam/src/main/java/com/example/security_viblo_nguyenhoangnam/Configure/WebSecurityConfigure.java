1dbe5dc90aa52a693bf11dc9e2e3a7e932240d36
PhanKhacDien <phankhacdien1110@gmail.com>
2023-06-18 19:01:12 +0700

    [UPDATE] Commit change from ABB laptop

--- Code JAVA/2. Java Web/02_Spring_Security/Security_Viblo_NguyenHoangNam/src/main/java/com/example/security_viblo_nguyenhoangnam/Configure/WebSecurityConfigure.java
+++ Code JAVA/2. Java Web/02_Spring_Security/Security_Viblo_NguyenHoangNam/src/main/java/com/example/security_viblo_nguyenhoangnam/Configure/WebSecurityConfigure.java
@@ -1,11 +1,53 @@
 package com.example.security_viblo_nguyenhoangnam.Configure;
 
+import com.example.security_viblo_nguyenhoangnam.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
-    
+
+    private UserService userService;
+
+    @Autowired
+    public WebSecurityConfigure(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/", "home").permitAll()   //Cho phép tất cả mọi người truy cập bình thường vào đường dẫn này.
+                    .anyRequest().authenticated()   //Tất cả các request còn lại đều phải được authenticated.
+                    .and()
+                .formLogin()    //sử dụng login form có sẵn của Spring security khi cần login
+                .defaultSuccessUrl("/hello").permitAll()    //Sau khi log-in thì được truy cập vào địa chỉ này
+                    .and()
+                .logout().permitAll();  //Cho phép log-out
+    }
 }
